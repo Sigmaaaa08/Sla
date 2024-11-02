1d3308ec0707cf350cb519f0ba7e19a94855f4c3
Sigmaaaa08 <123015338+Sigmaaaa08@users.noreply.github.com>
2024-11-02 00:48:12 -0300

    Update ex14.cpp
    Arrumei

--- ex14.cpp
+++ ex14.cpp
@@ -7,46 +7,46 @@ int main() {
 setlocale(LC_ALL, "");
 int op;
    float lado1, lado2, base, base2, area, altura, tri_equilatero, tri_retangulo, tri, quadrado, retangulo, trapezio, paralelogramo, hexagono, losango, apotema, perimetro, D, d;
-   cout<< "Menu de opções \n Areas: \n \n 1-Triângulo equilátero \n 2-Triângulo retângulo \n 3-Triângulo \n 4-Quadrado \n 5-Retângulo \n 6-Trapézio \n 7-Paralelogramo \n 8-Hexágono \n 9-Losango \n 0- Sair \n Escolha sua opção:";
+   cout<< "Menu de opÃ§Ãµes \n Areas: \n \n 1-TriÃ¢ngulo equilÃ¡tero \n 2-TriÃ¢ngulo retÃ¢ngulo \n 3-TriÃ¢ngulo \n 4-Quadrado \n 5-RetÃ¢ngulo \n 6-TrapÃ©zio \n 7-Paralelogramo \n 8-HexÃ¡gono \n 9-Losango \n 0- Sair \n Escolha sua opÃ§Ã£o:";
    cin>> op; 
    if(op==0){
    	return 0;
    }
    if(op==1){
-   	cout<< "Digite o lado do triangulo equilátero: ";
+   	cout<< "Digite o lado do triangulo equilÃ¡tero: ";
    	cin>> lado1;
    	tri_equilatero= ((lado1*lado1)* sqrt(3) / 4);
-   	cout<< "A area do triangulo equilatero é: " <<tri_equilatero<< "\n";
+   	cout<< "A area do triangulo equilatero Ã©: " <<tri_equilatero<< "\n";
    }   
    if(op==2){
-   	cout<< "Digite a altura do triângulo retângulo: ";
+   	cout<< "Digite a altura do triÃ¢ngulo retÃ¢ngulo: ";
    	cin>> altura;
-   	cout<< "Digite a base do triângulo retângulo: ";
+   	cout<< "Digite a base do triÃ¢ngulo retÃ¢ngulo: ";
    	cin>> base;
    	tri_retangulo= base*altura;
-   	cout<< "A area do triangulo equilatero é: " <<tri_retangulo<< "\n";
+   	cout<< "A area do triangulo equilatero Ã©: " <<tri_retangulo<< "\n";
    }   
     if(op==3){
-   	cout<< "Digite a altura do triângulo retângulo: ";
+   	cout<< "Digite a altura do triÃ¢ngulo retÃ¢ngulo: ";
    	cin>> altura;
-   	cout<< "Digite a base do triângulo: ";
+   	cout<< "Digite a base do triÃ¢ngulo: ";
    	cin>> base;
    	tri= (base*altura)/2;
-   	cout<< "A area do triangulo é: " <<tri<< "\n";
+   	cout<< "A area do triangulo Ã©: " <<tri<< "\n";
    }   
    if(op==4){
    	cout<< "Digite o lado do quadrado: ";
    	cin>> lado1;
    	quadrado= (lado1*lado1);
-   	cout<< "A area do quadrado é: " <<quadrado<< "\n";
+   	cout<< "A area do quadrado Ã©: " <<quadrado<< "\n";
    }   
    if(op==5){
-   	cout<< "Digite a altura do retângulo: ";
+   	cout<< "Digite a altura do retÃ¢ngulo: ";
    	cin>> altura;
-   	cout<< "Digite a base do retângulo: ";
+   	cout<< "Digite a base do retÃ¢ngulo: ";
    	cin>> base;
    	retangulo= base*altura;
-   	cout<< "A area do triangulo equilatero é: " <<tri_retangulo<< "\n";
+   	cout<< "A area do retÃ¢ngulo Ã©: " <<retangulo<< "\n";
    }   
     if(op==6){
    	cout<< "Digite a altura do trapezio: ";
@@ -56,7 +56,7 @@ int op;
    	cout<< "Digite a base menor do trapezio: ";
    	cin>> base2;
    	trapezio=(base+base2)*altura/2;
-   	cout<< "A area do triangulo equilatero é: " <<trapezio<< "\n";
+   	cout<< "A area do trapÃ©zio Ã©: " <<trapezio<< "\n";
    }  
    if(op==7){
    	cout<< "Digite a altura do paralelogramo : ";
@@ -64,16 +64,16 @@ int op;
    	cout<< "Digite a base do paralelogramo: ";
    	cin>> base;
    	tri= base*altura;
-   	cout<< "A area do triangulo é: " <<paralelogramo<< "\n";
+   	cout<< "A area do paralelogramo Ã©: " <<paralelogramo<< "\n";
    }    
    if(op==8){
-   	cout<< "Digite a apotema do hexágono: ";
+   	cout<< "Digite a apotema do hexÃ¡gono: ";
    	cin>> apotema;
-   	cout<< "Digite o lado do hexágono: ";
+   	cout<< "Digite o lado do hexÃ¡gono: ";
    	cin>> lado1;
    	perimetro=lado1*lado1;
    	hexagono=perimetro*apotema/2;
-   	cout<< "A area do triangulo é: " <<paralelogramo<< "\n";
+   	cout<< "A area do hexÃ¡gono Ã©: " <<hexagono<< "\n";
    }    
     if(op==9){
    	cout<< "Digite o D do losango: ";
@@ -81,7 +81,7 @@ int op;
    	cout<< "Digite o d: ";
    	cin>> d;
     losango=(D+d)/2;
-   	cout<< "A area do triangulo é: " <<losango<< "\n";
+   	cout<< "A area do losango Ã©: " <<losango<< "\n";
 }
 
    
