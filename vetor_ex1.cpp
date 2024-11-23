b1f7561c238a470a9fae145c81728a98c4e4255d
Karen <123015338+Sigmaaaa08@users.noreply.github.com>
2024-11-23 16:20:48 -0300

    Create vetor_ex1.cpp

---
+++ vetor_ex1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+setlocale(LC_ALL, " " );
+    int n[10] , cont = 0, i; 
+
+    cout << "Digite 10 números inteiros:\n";
+    for (int i = 1; i < 11; i++) {
+        cout << "Número " << i << ": ";
+        cin >> n[i];
+    }
+
+    for ( i = 0; i < 11; i++) {
+        if (n[i] > 50) {
+            cout << "Número: " << n[i] << ", Localização: " << i << "\n";
+            cout << "Números maiores que 50 e suas posições:\n";
+            cont++; 
+        }
+    }
+
+    if (cont == 0) {
+        cout << "Nenhum número maior que 50 foi encontrado.\n";
+    }
+
+    return 0;
+}
