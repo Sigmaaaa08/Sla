88aa4ec6058726721f7ba67ce7ba407bbaf95f59
Karen <123015338+Sigmaaaa08@users.noreply.github.com>
2024-11-25 09:10:26 -0300

    Update vetor_ex1.cpp

--- vetor_ex1.cpp
+++ vetor_ex1.cpp
@@ -3,25 +3,34 @@
 using namespace std;
 
 int main() {
-setlocale(LC_ALL, " " );
-    int n[10] , cont = 0, i; 
+    setlocale(LC_ALL, ""); 
 
-    cout << "Digite 10 números inteiros:\n";
-    for ( i = 1; i < 11; i++) {
-        cout << "Número " << i << ": ";
-        cin >> n[i];
-    }
+    int i = 0, qtd;
+    float nt1 = 0, nt2 = 0, nt3 = 0, mediap = 0;
 
-    for ( i = 1; i < 11; i++) {
-        if (n[i] > 50) {
-            cout << "Número: " << n[i] << ", Localização: " << i << "\n";
-            cout << "Números maiores que 50 e suas posições:\n";
-            cont++; 
+    cout << "Digite o numero de alunos para consultar a nota: ";
+    cin >> qtd;
+
+    for (i = 1; i <= qtd; i++) {
+        cout << "Digite a primeira nota do aluno " << i << ": ";
+        cin >> nt1;
+        cout << "Digite a segunda nota do aluno " << i << ": ";
+        cin >> nt2;
+        cout << "Digite a terceira nota do aluno " << i << ": ";
+        cin >> nt3;
+
+     
+        mediap = (nt1 * 3 + nt2 * 2 + nt3 * 5) / 10;
+
+        if (mediap >= 8) {
+            cout << "O aluno " << i << " ficou com menção MB\n";
+        } else if (mediap >= 7) {
+            cout << "O aluno " << i << " ficou com menção B\n";
+        } else if (mediap >= 6) {
+            cout << "O aluno " << i << " ficou com menção R\n";
+        } else {
+            cout << "O aluno " << i << " ficou com menção I\n";
         }
-    }
-
-    if (cont == 0) {
-        cout << "Nenhum número maior que 50 foi encontrado.\n";
     }
 
     return 0;
