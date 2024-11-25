94fefde0b74c732a19e78f5192dae1efe24da2f2
Karen <123015338+Sigmaaaa08@users.noreply.github.com>
2024-11-25 09:25:56 -0300

    Update ex6_rep.cpp

--- ex6_rep.cpp
+++ ex6_rep.cpp
@@ -1,37 +1,37 @@
 #include <iostream>
 #include <locale.h>
 using namespace std;
+
 int main() {
-setlocale(LC_ALL, "");
-    int i, qtd, nt1=2, nt2=3, nt3=5;
-    float x = 0, y = 0, z = 0, mediap;
+    setlocale(LC_ALL, ""); 
+
+    int i = 0, qtd;
+    float nt1 = 0, nt2 = 0, nt3 = 0, mediap = 0;
 
     cout << "Digite o numero de alunos para consultar a nota: ";
     cin >> qtd;
-    for (i = 1; i <= qtd ; i++){
-    cout << "Digite a primeira nota do aluno " <<i<< ": ";
-    cin>> x ;
-        cout << "Digite a segunda nota do aluno " <<i<<": ";
-        cin>> y ;
-        cout << "Digite a terceira nota do aluno " <<i<<": " ;
-        cin>> z;
-        
-        //media ponderada:
-        mediap = x + y + z / nt1 + nt2 + nt3 ;
-        
- if (mediap>=8 || media==10){
-     cout << "O aluno" <<i<< "ficou com" "menção MB";
- }
- else if (mediap>=7 ||  media==8){
-     cout << "O aluno" <<i<< "ficou com" "menção B";
- }
-  else if (mediap>=6 || media==7){
-    cout << "O aluno" <<i<< "ficou com" "menção R";
- }
-  else if (mediap>=0 || media==6){
-    cout << "O aluno" <<i<< "ficou com" "menção I";
- }
 
-        
+    for (i = 1; i <= qtd; i++) {
+        cout << "Digite a primeira nota do aluno " << i << ": ";
+        cin >> nt1;
+        cout << "Digite a segunda nota do aluno " << i << ": ";
+        cin >> nt2;
+        cout << "Digite a terceira nota do aluno " << i << ": ";
+        cin >> nt3;
+
+     
+        mediap = (nt1 * 3 + nt2 * 2 + nt3 * 5) / 10;
+
+        if (mediap >= 8 || mediap == 10 ) {
+            cout << "O aluno " << i << " ficou com menção MB\n";
+        } else if (mediap >= 7 || mediap == 8) {
+            cout << "O aluno " << i << " ficou com menção B\n";
+        } else if (mediap >= 6 || mediap == 7) {
+            cout << "O aluno " << i << " ficou com menção R\n";
+        } else if (mediap< 6 ) {
+            cout << "O aluno " << i << " ficou com menção I\n";
+        }
+    }
+
     return 0;
 }
